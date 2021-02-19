c76d981eb4f1d82cf3dc1bcce7384680ab8555c3
Sean Dahan <aui.svi@gmail.com>
2021-02-19 23:35:49 +0200

    Css Crash Course and Index.js
    Got some early progress on css crash course and learned some java script on websites

---
+++ Html CheatSheet/index.js
@@ -0,0 +1,12 @@
+let person = {
+    name : "Sean Dahan",
+    age : 16
+};
+let colors = ['red','blue'];
+let returnVar = greet("Sean Dahan");
+console.log(returnVar);
+function greet(name) {
+    //let name = "Sean Dahan";
+    console.log(name);
+    return 16;
+}
